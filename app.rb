a3dde8f8d7ec6bab074db9f57431c5d8c5e07005
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-30 07:57:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,15 +2,25 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/reverse' do
+    
     erb :reverse
   end
 
   post '/reverse' do
+    original_string = params["string"]
+  @reversed_string = original_string.reverse
+ 
     erb :reversed
   end
 
-  get '/friends' do
-    # Write your code here!
 
-  end
+    # Write your code here!
+  get '/friends' do
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+ 
+    erb :friends
+      <% @friends.each do 
+      |friend| %>
+      <h2><%= friend %></h2>
+   <%end%>
 end
